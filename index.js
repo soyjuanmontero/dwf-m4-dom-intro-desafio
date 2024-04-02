c170fff3f4e6dabd9a3582188ff6da9d861e8aa7
Juan Manuel Montero <juanmanuel231295@gmail.com>
2024-04-02 11:40:59 -0300

    desafio Dom terminado

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listasEl = document.querySelectorAll(".lista li");
+  const lista = document.querySelector(".lista");
+  listasEl.forEach((e) => {
+    e.remove();
+  });
+
+  for (const el of cosasQueAprendimos) {
+    console.log(el);
+    const liEl = document.createElement("li");
+    liEl.textContent = el.tema;
+    if (el.class == "special") {
+      liEl.classList.add(el.class);
+    } else {
+    }
+    lista.appendChild(liEl);
+  }
+}
 
 main();
